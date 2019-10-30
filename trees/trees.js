9edb05a5b5c7b8f27e98c14333b6e9862b3b2ba8
phillip-eth <54546971+phillip-eth@users.noreply.github.com>
2019-10-30 11:47:16 -0400

    contains and size

--- trees/trees.js
+++ trees/trees.js
@@ -23,13 +23,38 @@ class BTNode {
     }
     }
     min(){
-        console.log(this.val);
+        
         if(this.left==null){
             return this.val;
         }else{
-            console.log(this.left.val);
+            
             return this.left.min();
         }
+    }
+
+    count(){
+        var counter=1;
+        if(this.right!=null){
+            var rightCount= this.right.count();
+            counter+=rightCount;
+        }
+        if(this.left!=null){
+            var leftCount=this.left.count();
+            counter+=leftCount;
+        }
+        return counter;
+    }
+    search(val){
+        if(this.val == val){
+            return true;
+        }
+        if(val<this.val && this.left!=null){
+            return this.left.search(val);
+        }
+        if(val> this.val && this.right!=null){
+            return this.right.search(val)
+        }
+        return false;
     }
     
 }
@@ -101,6 +126,20 @@ class BST {
             return this.root.min();
         }
     }
+    size(){
+        if(this.root==null){
+            return 0;
+        }else{
+            return this.root.count();
+        }
+    }
+    contains(val){
+        if(this.root==null){
+            return false;
+        }else{
+            return this.root.search(val);
+        }
+    }
 }
     
 
@@ -109,11 +148,19 @@ bSTree=new BST();
 bSTree.newAdd(10);
 bSTree.newAdd(35);
 bSTree.newAdd(5);
+bSTree.newAdd(7);
 bSTree.newAdd(3);
-// console.log(bSTree.root.right.val);
-// console.log(bSTree.root.left.val);
-// console.log(bSTree.root.val);
+bSTree.newAdd(19);
+bSTree.newAdd(255);
+bSTree.newAdd(15);
+bSTree.newAdd(-3);
+bSTree.newAdd(8);
+console.log(bSTree.root.right.val);
+console.log(bSTree.root.left.val);
+console.log(bSTree.root.val);
 console.log("The min is:"+bSTree.min2());
+console.log(bSTree.size());
+console.log(bSTree.contains(-35));
 
 
 
