013a658f7794e692f1fa5b0aea24f453cd1faa1a
phillip-eth <54546971+phillip-eth@users.noreply.github.com>
2019-11-21 11:15:55 -0600

    hashes

--- hashes/hashes.js
+++ hashes/hashes.js
@@ -57,10 +57,16 @@ class HashMap {
   findKey(key){
     const testKey = this.generateHashKey(key)
     if(!this.table[testKey]){
-      return false;
+      return null;
+    }else{
+      for( let k in this.table[testKey]){
+        if(key == this.table[testKey][k][0]){
+
+          return this.table[testKey][k];
+        }
+      }
     }
 
-    return this.table[testKey];
   }
 
   remove(key){
@@ -71,7 +77,7 @@ class HashMap {
     }
     for( let i=0; i<this.table[testKey].length; i++){
       if( key == this.table[testKey][i][0] ){
-        console.log(this.table[testKey][i]);
+        // console.log(this.table[testKey][i]);
          temp = this.table[testKey].splice(i, 1);
          this.entries-=1;
       }
@@ -81,41 +87,90 @@ class HashMap {
 
 
   hashGrow(){
-    this.capacity = Math.floor(this.capacity * 1.5);
-    let oldTable = this.table;
-    this.table=[];
-    this.entries = 0;
-    for (const entry of oldTable){
+    return this.setSize(Math.floor(this.capacity*1.5));
+  }
+
+
+  addMap(map){
+    if(!map.entries){
+      return this;
+    }
+    for (const entry of map.table){
       if(entry){
         for(const [key, value] of entry){
           this.add(key, value);
         }
-
       }
     }
     return this;
   }
+
+  loadFactor(){
+    return (this.entries / this.capacity)*100;
+  }
+
+  setSize(newCap){
+    // console.log(this.entries, newCap);
+    if(newCap>0){
+      let oldTable = this.table;
+      this.table=[];
+      this.entries = 0;
+      this.capacity = newCap;
+      for (const entry of oldTable){
+        if(entry){
+          for(const [key, value] of entry){
+            this.add(key, value);
+          }
+        }
+      }
+      return this;
+    }else {
+      throw new Error("Please set the size to more than 0.");
+    }
+  }
+
+  selectKeys(keys){
+
+    let newTable =[];
+    for(const key of keys){
+      let testKey =  this.findKey(key);
+      if(testKey){
+        newTable.push(testKey);
+      }
+    }
+    console.log(newTable);
+    this.entries = 0;
+    this.table=[];
+    for(let [key,value] of newTable){
+      this.add(key,value)
+    }
+    console.log(this.table)
+    return this;
+  }
+
 }
+
+// Create method selectKeys(keyArray) to accepts an array of keys. Reject those keys in the existing map that are NOT in that array. If your map contains {"cool":"Pariece", "smart":"Pariece", "tall":"Kareem"} , then map.selectKeys(["cool","smart"]) should change map to {"cool":"Pariece", "smart":"Pariece"} .
+
 
 
 const map = new HashMap(15);
 const map2 = new HashMap(50);
 
-map.add("cat", "hat").add("test","mean").add("table","chair").add("cat","sunshine")
+map.add("cat", "hat").add("test","mean").add("table","chair").add("cat","sunshine").add("cup","coffee").add("hot","cold")
 
-// console.log(map.contains('frank'));
-// console.log(map2.isEmpty());
+map2.add("sun","glasses").add("snow","shoes").add("fall","leaves").add("spring","sprung")
 
-// console.log(map.findKey('test'));
+map.addMap(map2);
+// console.log(map.table);
+// console.log(map.loadFactor())
 
-// console.log("Table length is: "+map.table.length +", and the table contents are: "+ map.table);
+// // map.setSize(-5);
 
-// console.log(map.table)
-console.log(map.table);
-console.log(map.entries);
-console.log(map.capacity);
-map.hashGrow();
-console.log(map.capacity);
+// console.log(map.table,map.entries);
+
+map.selectKeys(['cat','test','cup']);
+
+// console.log(map.table, map.entries);
 
 
-console.log(map.table);
